8e94e6ef3b2a205404ecec498c7bd7261327f4c9
Jerry Chen <s.chen0086@gmail.com>
2026-09-12 01:24:29 +0100

    Home page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,9 @@ import {
   redirect,
 } from "next/navigation";
 
-import {
-  createClient,
-} from "@/lib/supabase/server";
 
-
-export default async function HomePage() {
-  const supabase =
-    await createClient();
-
-
-  const {
-    data,
-  } =
-    await supabase.auth
-      .getClaims();
-
-
-  if (
-    data?.claims
-  ) {
-    redirect(
-      "/dashboard"
-    );
-  }
-
-
+export default function HomePage() {
   redirect(
-    "/login"
+    "/dashboard"
   );
 }
